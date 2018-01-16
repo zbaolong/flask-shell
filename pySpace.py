c23ec9934312e07471862d6b5f881ce6cc01c505
root <root@192.168.2.188>
2018-01-16 10:56:16 +0800

    完成测试

--- pySpace.py
+++ pySpace.py
@@ -30,10 +30,7 @@ basePath = os.path.dirname(os.path.realpath(__file__))
 
 
 def execShell(file):
-    hello_sh = basePath + file
-    print hello_sh
-    result_lines = os.popen('sh ' + hello_sh, 'rU').readlines()
-    print "方法结果", result_lines
+    result_lines = os.popen('sh ' + file).readlines()
     return result_lines
 
 
@@ -41,22 +38,31 @@ def execShell(file):
 def hello_html():
     return app.send_static_file("html/hello.html")
 
+def resultResponse(result_lines):
+    decodeList = []
+    for line in result_lines:
+        decodeList.append(line.replace("\n", ""))
+    return decodeList
+
+
 
 @app.route('/<name>', methods=['GET'])
 def hello_world(name):
+    result_lines = "nothing to"
+    file = "/shell/hello.sh"
     if name == "app":
-        print "app"
-        return "app"
-    elif name == "html":
-        print "html"
-        return "html"
+        file = "/root/bin/start-app"
+    elif name == "vue":
+        file = "/root/bin/start-vue"
+    elif name == "oa":
+        file = "/root/bin/start-oa"
+    elif name == "nginx":
+        file = "/root/bin/start-nginx"
     else:
         file = '/shell/hello.sh'
-        result_lines = execShell(file)
-        decodeList = []
-        for line in result_lines:
-            decodeList.append(line.replace("\n", ""))
-    return decodeList
+        file = basePath + file
+    result_lines = execShell(file)
+    return resultResponse(result_lines)
 
 
 if __name__ == '__main__':
