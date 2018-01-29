c6531dee05d46be7b4028a5e3063a941f8cb04f7
yyh <673677097@qq.com>
2018-01-29 22:52:29 +0800

    'websocketio'

--- pySpace.py
+++ pySpace.py
@@ -2,23 +2,27 @@
 import os
 import sys
 
-from flask import Flask
 from flask import Response
 from flask import jsonify
-
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 # 设置默认编码
-# default_encoding = "utf-8"
-# if (default_encoding != sys.getdefaultencoding()):
-#     reload(sys)
-#     sys.setdefaultencoding(default_encoding)
+default_encoding = "utf-8"
+if (default_encoding != sys.getdefaultencoding()):
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
 
 
 class MyResponse(Response):
     @classmethod
     def force_type(cls, response, environ=None):
         if isinstance(response, (list, dict)):
-            response = jsonify(response)
+            try:
+                response = jsonify(response)
+            except RuntimeError:
+                print('返回的字符串不是有效json')
+
         return super(Response, cls).force_type(response, environ)
 
 
@@ -27,6 +31,12 @@ app = Flask(__name__, static_url_path="")
 app.response_class = MyResponse
 
 basePath = os.path.dirname(os.path.realpath(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+socketio = SocketIO(app)
+
+
+# app.config['SECRET_KEY'] = 'secret!'
 
 
 def execShell(file):
@@ -38,12 +48,19 @@ def execShell(file):
 def hello_html():
     return app.send_static_file("html/hello.html")
 
+
+@socketio.on('connect', namespace='shell')
+def wsConnect():
+    print('>>> websocket connected')
+
+
 def resultResponse(result_lines):
     decodeList = []
     for line in result_lines:
-        decodeList.append(line.replace("\n", ""))
+        row = line.replace("\n", "")
+        decodeList.append(row)
+        socketio.emit('message', {'data': row})
     return decodeList
-
 
 
 @app.route('/<name>', methods=['GET'])
@@ -66,4 +83,6 @@ def hello_world(name):
 
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8088)
+    # app.run('0.0.0.0', 8088)
+    socketio.run(app, '0.0.0.0', 8088, debug='true')
+    # socketio.run(app)
--- shell/hello.sh
+++ shell/hello.sh
@@ -2,3 +2,6 @@
 echo "hello world"
 echo 'ahahahhahhah'
 echo  `ls`
+echo  `ls`
+echo  `ls`
+echo  `ls`
